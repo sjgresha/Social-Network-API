221dea0c53ea4bf1e31133e5e864546ade9e08b2
Steven Gresham <sbjg2545@gmail.com>
2023-11-13 19:28:07 -0700

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+//Boiler Plate
+const { connect, connection } = require('mongoose');
+
+// After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
+// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
+const connectionString =
+  process.env.MONGODB_URI || //'mongodb://127.0.0.1:27017/socialApiDB'; //Need to change to mine
+
+connect(connectionString);
+
+module.exports = connection;
